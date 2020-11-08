45c32ffc59180bc06d8d3d76c5d53b2152ae6891
piotrpawlowski7 <74098973+piotrpawlowski7@users.noreply.github.com>
2020-11-08 22:09:14 +0100

    nowy baner

--- first.js
+++ first.js
@@ -1,1 +1,8 @@
-console.log('hejka');
+var then = new Date(2020, 11, 08), // month is zero based
+    now  = new Date;               // no arguments -> current date
+
+// 24 hours, 60 minutes, 60 seconds, 1000 milliseconds
+Math.round((now - then) / (1000 * 60 * 60 * 24)); // round the amount of days
+// result: 712
+
+print now
